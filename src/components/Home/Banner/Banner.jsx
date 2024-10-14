0dae97de4713ffa117568909656c57b1d39e16ce
Shafiulnobel <alfrednobel636@gmail.com>
2024-10-14 22:39:00 +0600

    update color

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className='container px-4 mx-auto'>
             <div class="grid grid-cols-1 md:grid-cols-2 items-center my-24">
                 <div className=''>
-                    <h3 className='roboto-medium leading-relaxed md:leading-relaxed text-3xl md:text-4xl my-10 '>
+                    <h3 style={{color: "#1a1a1a"}} className='roboto-medium leading-relaxed md:leading-relaxed text-3xl md:text-4xl my-10 '>
                         I’m Nobel, a <span className='text-blue-600/100'>Product Designer</span> . I enjoy creating user-centric, delightful, and human experiences.
                     </h3>
                 </div>
